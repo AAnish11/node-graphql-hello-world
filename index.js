4f4439dba5cb37f0bbb48216aeb2f1093738ee45
Anish Agarwal <anish.agarwal2010@gmail.com>
2021-08-23 18:10:39 +0530

    mutation exmaple revert

--- index.js
+++ index.js
@@ -1,50 +1,64 @@
+const { buildSchema } = require('graphql');
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
-const graphql = require('graphql');
 
-const existingDatabase = {
-    1: {
-        name: 'Anish Agarwal',
-        mobileNumber: '+91XXXXXXXXXXXXXX'
+let messageId = 1;
+let messages = {};
+// Used String!, It means a function not return nullable value
+const schemaData = `
+input MessageInput {
+    content: String!,
+    author: String!
+}
+type Message {
+    id: ID!
+    content: String!,
+    author: String!
+}
+type Mutation {
+    createMessage(inputData: MessageInput!): Message,
+    updateMessage(id: ID!, inputData: MessageInput!): Message,
+}
+type Query {
+    getAllMessages: [Message!],
+    getMessageById(id: ID!): Message!
+}
+`;
+// Create build schema for hello world program
+const schema = buildSchema(schemaData);
+// root value
+const root = {
+    getAllMessages: () => {
+        return Object.values(messages).map(msg => new Message(msg.id, msg.content, msg.author));
     },
-    2: {
-        name: 'Heena Agarwal',
-        mobileNumber: '+91XXXXXXXXXXXXXX'
+    getMessageById: ({ id }) => {
+        return messages[id] || {};
+    },
+    createMessage: (data) => {
+        const { author, content } = data.inputData;
+        const newMessage = new Message(messageId, content, author);
+        messages[messageId] = newMessage;
+        messageId += 1;
+        return newMessage;
+    },
+    updateMessage: (data) => {
+        messageId = data.id;
+        const { author, content } = data.inputData;
+        const updatedMessage = new Message(messageId, content, author);
+        messages[messageId] = updatedMessage;
+        return updatedMessage;
     }
 };
-
-// create graphql type for 
-const userType = new graphql.GraphQLObjectType({
-    name: 'user',
-    fields: {
-        name: {
-            type: graphql.GraphQLString,
-        },
-        mobileNumber: {
-            type: graphql.GraphQLString
-        }
+// Class for createing new object for messahe
+class Message {
+    constructor(id, content, author) {
+        this.id = id;
+        this.content = content;
+        this.author = author;
     }
-});
+}
 
-const queryType = new graphql.GraphQLObjectType({
-    name: 'Query',
-    fields: {
-        user: {
-            type: userType,
-            args: {
-                id: {
-                    type: graphql.GraphQLString
-                }
-            },
-            resolve: (_, { id }) => {
-                console.log(id);
-                return existingDatabase[id];
-            }
-        },
-    }
-});
 
-const schema = new graphql.GraphQLSchema({ query: queryType });
 
 // create app instance
 const app = express();
@@ -53,6 +67,7 @@ const app = express();
 
 app.use('/', graphqlHTTP({
     schema,
+    rootValue: root,
     graphiql: true,
 }));
 // define port
